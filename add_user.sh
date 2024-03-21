aa9f177710f5fd3b3203aec600377a6fe11c8ce7
Gaiiaalessio <146933911+Gaiiaalessio@users.noreply.github.com>
2024-03-21 16:42:01 +0100

    Update add_user.sh

--- add_user.sh
+++ add_user.sh
@@ -2,11 +2,11 @@
 
 URL="http://localhost:8080/ocs/v1.php/cloud/users"
 USERNAME="Gaiiaalessio"
-PASSWORD="Francesca26!"
+PASSWORD="Admin"
 PASSWORD_ENCODED="abc123abc!" 
 
 
-for i in {11..30}; do
+for i in {1..30}; do
     USERID="user$i"
     docker exec -i -u 33 gaialessio-app-1 bash -c "export OC_PASS=$PASSWORD_ENCODED && /var/www/html/occ user:add $USERID --password-from-env"
 done
